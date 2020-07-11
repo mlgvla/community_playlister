0cc20e72b41481df0a2724206798415cda83c146
mlgvla <mlgerardvla@gmail.com>
2020-07-11 14:03:27 -0400

    Added Playlist Index page using Bootstrap table feature

---
+++ app/controllers/playlists_controller.rb
@@ -0,0 +1,23 @@
+class PlaylistsController < ApplicationController
+    def index
+        @playlists = Playlist.all
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_160157) do
+ActiveRecord::Schema.define(version: 2020_07_11_165046) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_07_11_160157) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "description"
     t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
