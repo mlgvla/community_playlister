9d71ebe42f680fb6f2c884120985b740469fbba9
mlgvla <mlgerardvla@gmail.com>
2020-07-11 12:04:02 -0400

    Added user foreign key to playlists table

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,4 +2,5 @@ class Playlist < ApplicationRecord
     has_many :playlist_tracks
     has_many :tracks, through: :playlist_tracks
     has_many :comments
+    belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_212215) do
+ActiveRecord::Schema.define(version: 2020_07_11_160157) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2020_07_07_212215) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
   create_table "tracks", force: :cascade do |t|
