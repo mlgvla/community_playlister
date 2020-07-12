8068ba368dbab81546f0511d7ad3ed715b244036
mlgvla <mlgerardvla@gmail.com>
2020-07-11 23:47:48 -0400

    Adding Track attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_165046) do
+ActiveRecord::Schema.define(version: 2020_07_12_033342) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -40,13 +40,20 @@ ActiveRecord::Schema.define(version: 2020_07_11_165046) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.text "description"
+    t.boolean "public", default: false
     t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
   create_table "tracks", force: :cascade do |t|
-    t.string "title"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "artist_id"
+    t.integer "album_id"
+    t.integer "duration"
+    t.string "preview_url"
+    t.index ["album_id"], name: "index_tracks_on_album_id"
+    t.index ["artist_id"], name: "index_tracks_on_artist_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,16 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
+
 #   Character.create(name: 'Luke', movie: movies.first)
 
 users = User.create([{ first_name: "Monica", last_name: "Gerard", email: "monica@mail.com", password: "pwmg" }, { first_name: "Tom", last_name: "Sefcovic", email: "tom@mail.com", password:  "pwts"}, { first_name: "Nate" , last_name: "Sefcovic", email: "nate@mail.com", password: "ptns" }, { first_name: "Seyr", last_name: "Sefcovic", email: "seyr@mail.com", password: "ptss" }, { first_name: "Amanda", last_name: "D'Autrechey", email: "amanda@mail.com", password: "ptad" }, { first_name: "John", last_name: "Sefcovic", email: "john@mail.com", password: "pwjs"}, { first_name: "Lois", last_name: "Sefcovic", email: "lois@mail.com", password: "pwls" }, { first_name: "Ingrid", last_name: "Cohen", email: "ingrid@mail.com", password: "pwic" }, { first_name: "Merle", last_name: "Keitel", email: "merle@mail.com", password: "pwmk" }, { first_name: "Dale", last_name: "Blumenthal", email: "dale@mail.com", password: "pwdb" }])
 
-tracks = Track.create([{ title: "Cats are Awesome" }, { title: "Dogs are Awesome" }, { title: "Birds are Amazing" }, { title: "I Like Aardvarks" }, { title: "Snakes are Slithery" }, { title: "Fishes Glug" }, { title: "Cows Go Moo" }, { title: "Tacos are Real" }, { title: "Mozart is Groovy" }, { title: "Viola Rules" }])
+tracks = Track.create([{ title: "Cats are Awesome", artist_id: , album_id: , duration_ms: , preview_url: "blob" }, { title: "Dogs are Awesome", artist_id: , album_id: , duration_ms: , preview_url: "blob" }, { title: "Birds are Amazing", artist_id: , album_id: , duration_ms: , preview_url: "blob" }, { title: "I Like Aardvarks", artist_id: , album_id: , duration_ms: , preview_url: "blob" }, { title: "Snakes are Slithery", artist_id: , album_id: , duration_ms: , preview_url: "blob" }, { title: "Fishes Glug", artist_id: , album_id: , duration_ms: , preview_url: "blob" }, { title: "Cows Go Moo", artist_id: , album_id: , duration_ms: , preview_url: "blob" }, { title: "Tacos are Real", artist_id: , album_id: , duration_ms: , preview_url: "blob" }, { title: "Mozart is Groovy", artist_id: , album_id: , duration_ms: , preview_url: "blob" }, { title: "Viola Rules", artist_id: , album_id: , duration_ms: , preview_url: "blob" }])
 
 playlists = Playlist.create([{ name: "Animals Toonz" }, { name: "Weird Pets" }, { name: "Music Things" }, { name: "Aquarium Guys" }, { name: "Classical Gas" }, { name: "House Pets" }, { name: "Four Legged Things" },{ name: "Flying Things" },{ name: "Weird Animals" },{ name: "Mexican Things" }])
 
+albums = 
+
+artist = 
