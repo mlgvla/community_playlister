8351430f589b3ad47b7dc3ed2b934b7edd1c953f
mlgvla <mlgerardvla@gmail.com>
2020-07-12 23:25:00 -0400

    Added user/playlist nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,25 @@
 Rails.application.routes.draw do
 
-  root 'sessions#welcome'
+    root 'sessions#welcome'
 
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+    get '/signup', to: 'users#new'
+    post '/signup', to: 'users#create'
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
 
-  get '/auth/:provider/callback', to: 'sessions#omniauth'
+    get '/auth/:provider/callback', to: 'sessions#omniauth'
 
-  resources :users, only: [:show]
-  resources :playlists, only: [:index, :new, :create, :show, :edit]
+    
+
+    resources :users, only: [:show]
+    resources :playlists, only: [:index, :new, :create, :show, :edit, :destroy]
+
+    resources :users do
+        resources :playlists, shallow: true
+    end
+
+    
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
