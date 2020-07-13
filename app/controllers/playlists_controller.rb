b14c65f89242f142c673b5cf2f6599ed901a8464
mlgvla <mlgerardvla@gmail.com>
2020-07-12 23:22:55 -0400

    Added nested resource to index and update action

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,6 +1,12 @@
 class PlaylistsController < ApplicationController
+
     def index
-        @playlists = Playlist.all
+        if params[:user_id]
+            @user = User.find_by_id(params[:user_id])
+            @playlists = @user.playlists.uniq
+        else
+            @playlists = Playlist.all
+        end
 	end
 
     def new
@@ -16,25 +22,34 @@ class PlaylistsController < ApplicationController
 
     def show
         @playlist = Playlist.find_by_id(params[:id])
-        @tracks = Track.all
+        @playlist_tracks = PlaylistTrack.where(user_id: current_user.id, playlist_id: @playlist.id)
         if !@playlist
             flash[:error] = "Playlist not found."
             redirect_to playlists_path
         end          
 	end
 
-	def edit
-	end
+    def edit
+        @playlist = Playlist.find_by_id(params[:id])
+    end
 
-	def update
-	end
+    def update
+        @playlist = Playlist.find_by_id(params[:id])
+        if @playlist.update(playlist_params)
+            redirect_to playlist_path(@playlist)   
+        else
+            render :edit 
+        end   
+    end
 
-	def destroy
+    def destroy
+        Playlist.find(params[:id]).destroy
+        redirect_to playlists_path
     end
     
     private
 
     def playlist_params
-       params.require(:playlist).permit(:name, :description) 
+       params.require(:playlist).permit(:name, :description, :user_id, :public)
     end
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,5 @@
 class Playlist < ApplicationRecord
-    has_many :playlist_tracks
+    has_many :playlist_tracks, dependent: :destroy
     has_many :tracks, through: :playlist_tracks
     has_many :comments
     belongs_to :user
