626397af1bb5c6070f6d0fbc5d2f2d228a1c6044
mlgvla <mlgerardvla@gmail.com>
2020-07-13 23:10:42 -0400

    Added public_playlists scope method

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,4 +3,9 @@ class Playlist < ApplicationRecord
     has_many :tracks, through: :playlist_tracks
     has_many :comments
     belongs_to :user
+
+    #add validations for presence of name and user_id
+
+    #scope - alphabetize, current length of playlist?, 
+    scope :public_playlists ,-> { where( public: true )}
 end
